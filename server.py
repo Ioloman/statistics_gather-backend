522d416392402cf146b95d5778406ab519d480da
Kirill <kirilldd2@yandex.ru>
2022-04-05 21:43:47 +0700

    fix;

--- server.py
+++ server.py
@@ -1,44 +1,15 @@
 import os
-import os.path
 from databases import Database
 from tornado.ioloop import IOLoop
 import tornado.web
 from tornado.httpserver import HTTPServer
-from requests_app.handlers import (
-    RequestAddHandler, 
-    RequestGetHandler, 
-    RequestDeleteHandler, 
-    RequestUpdateHandler, 
-    GetStaticticsHandler,
-    MainPageHandler
-)
 
 
 
 def make_app(database: Database):
     # pass connection to each handler
     return tornado.web.Application([
-        tornado.web.url(
-            r'/api/add', RequestAddHandler, {'connection': database.connection()}, name='add'
-        ),
-        tornado.web.url(
-            r'/api/get', RequestGetHandler, {'connection': database.connection()}, name='get'
-        ),
-        tornado.web.url(
-            r'/api/remove', RequestDeleteHandler, {'connection': database.connection()}, name='delete'
-        ),
-        tornado.web.url(
-            r'/api/update', RequestUpdateHandler, {'connection': database.connection()}, name='update'
-        ),
-        tornado.web.url(
-            r'/api/statistic', GetStaticticsHandler, {'connection': database.connection()}, name='stats'
-        ),
-        tornado.web.url(
-            r'/', tornado.web.RedirectHandler, {'url': '/api'}
-        ),
-        tornado.web.url(
-            r'/api', MainPageHandler, name='main'
-        ),
+
     ])
 
 
